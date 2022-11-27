2b15b3c04ff0e8372ce67055b01e0d5796939183
Tyler Charro <106177945+NathanTyle@users.noreply.github.com>
2022-11-27 17:50:29 -0600

    added router.use and exports to api index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,3 +2,7 @@ const router = require('express').Router();
 const thoughtRoutes = require('./thought-routes');
 const userRoutes = require('./user-routes');
 
+router.use('/user', userRoutes);
+router.use('/thought', thoughtRoutes);
+
+module.exports = router;
