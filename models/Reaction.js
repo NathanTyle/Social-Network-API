b11bfd7d84ad18ad12b2e637fa87485df7593cf7
Tyler Charro <106177945+NathanTyle@users.noreply.github.com>
2022-11-28 16:24:47 -0600

    added requires to Reactions.js

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,27 +1,6 @@
 const { TopologyDescription } = require('mongodb');
 const { schema, Types, Schema } = require('mongoose');
 
-const reactionSchema = new Schema(
-    {
-        reactionId: {
-            type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId(),
-        },
-        reactionBody: {
-            type: String,
-            required: true,
-            max_length: 300,
-        },
-        username: {
-            type: String,
-            required: true,
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-        },
-    },
-);
 
 // Reaction (SCHEMA ONLY)
 
