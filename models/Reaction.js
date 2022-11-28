4c34c6a702810bc187e9fe556b20598c155f7dcd
Tyler Charro <106177945+NathanTyle@users.noreply.github.com>
2022-11-27 20:32:48 -0600

    added reactionSchema functionality

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,3 +1,26 @@
-const { schema, Types } = require('mongoose');
+const { TopologyDescription } = require('mongodb');
+const { schema, Types, Schema } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            max_length: 300,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+    },
+);
 
 module.exports = reactionSchema;
--- models/User.js
+++ models/User.js
@@ -1,3 +1,24 @@
 const { Schema, model } = require('mongoose');
 
+
+// User
+
+// username
+
+// String
+// Unique
+// Required
+// Trimmed
+// email
+
+// String
+// Required
+// Unique
+// Must match a valid email address (look into Mongoose's matching validation)
+// thoughts
+
+// Array of _id values referencing the Thought model
+// friends
+
+// Array of _id values referencing the User model (self-reference)
 module.exports = User;
