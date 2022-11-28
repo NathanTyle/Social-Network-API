63194fde4a5ea782bd0fc640637203e881bb6a44
Tyler Charro <106177945+NathanTyle@users.noreply.github.com>
2022-11-28 15:44:14 -0600

    cleaned up User model

--- models/User.js
+++ models/User.js
@@ -41,15 +41,4 @@ userSchema.virtual('friendCount').get(function() {
 
 const User = model('User', userSchema);
 
-// thoughts
-
-// Array of _id values referencing the Thought model
-
-// friends
-
-// Array of _id values referencing the User model (self-reference)
-
-// Schema Settings
-
-// Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
 module.exports = User;
