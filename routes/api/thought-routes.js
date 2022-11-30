e7b5d6b29b0272a7089a448a4485c2973f17bcad
Tyler Charro <106177945+NathanTyle@users.noreply.github.com>
2022-11-30 15:00:19 -0600

    added module exports

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -14,3 +14,5 @@ router.route('/').get(getRoutes).post(createThought);
 router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
 
 router.route('/:thoughtId/reactions').post(createReaction).delete(deleteReaction);
+
+module.exports = router;
