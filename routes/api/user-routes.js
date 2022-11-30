1a82c24232dd6d91e2cc937eacb5d7c110735ef6
Tyler Charro <106177945+NathanTyle@users.noreply.github.com>
2022-11-30 15:10:29 -0600

    added module exports

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -15,3 +15,5 @@ router.route('/').get(getUsers).post(createUser);
 router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
 
 router.route('/:userId/friends').post(createFriend).delete(deleteFriend);
+
+module.exports = router;
