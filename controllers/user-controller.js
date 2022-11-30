3e4101c764e2a8851bcda2fb4f913d9894e173eb
Tyler Charro <106177945+NathanTyle@users.noreply.github.com>
2022-11-29 21:50:49 -0600

    added get one user functionality

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -7,5 +7,16 @@ module.exports = {
         .catch((err) => res.status(500).json(err));
     },
 
+    getSingleUser(req, res) {
+        User.findOne({ _id: req.params.userId })
+        .select('-_V')
+        .then((user) =>
+        !user
+        ?res.status(404).json({'Sorry but there is no User with this ID'})
+        :res.json(user)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
+
     
 }
