d9c0cffc72b0127cd636d90041d9338067494734
Tyler Charro <106177945+NathanTyle@users.noreply.github.com>
2022-11-30 14:47:53 -0600

    added delete friends functionality

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -60,7 +60,7 @@ module.exports = {
         )
         .then((thought) =>
         !thought
-        ? res.status(404).json({ message: 'Sorry but there is no Thought with this ID'})
+        ? res.status(404).json({ message: 'Sorry but there is no Reaction with this ID'})
         : res.json(thought))
         .catch((err) => res.status(500).json(err));
     },
@@ -73,7 +73,7 @@ module.exports = {
         )
         .then(thought =>
             !thought
-            ? res.status(404).json({ message: 'Sorry but there is no Thought with this ID'})
+            ? res.status(404).json({ message: 'Sorry but there is no Reaction with this ID'})
             : res.json(thought))
             .catch((err) => res.status(500).json(err));
     }
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -60,24 +60,21 @@ module.exports = {
         )
         .then((user) =>
         !user
-        ?res.status(404).json({ message: 'Sorry but there is no User with this ID'})
+        ?res.status(404).json({ message: 'Sorry but there is no Friend with this ID'})
         : res.json(user))
         .catch((err) => res.status(500).json(err));
     },
 
-    
-
-    // AS A social media startup
-    // I WANT an API for my social network that uses a NoSQL database
-    // SO THAT my website can handle large amounts of unstructured data
-    // Acceptance Criteria
-    // GIVEN a social network API
-    // WHEN I enter the command to invoke the application
-    // THEN my server is started and the Mongoose models are synced to the MongoDB database
-    // WHEN I open API GET routes in Insomnia for users and thoughts
-    // THEN the data for each of these routes is displayed in a formatted JSON
-    // WHEN I test API POST, PUT, and DELETE routes in Insomnia
-    // THEN I am able to successfully create, update, and delete users and thoughts in my database
-    // WHEN I test API POST and DELETE routes in Insomnia
-    // THEN I am able to successfully create and delete reactions to thoughts and add and remove friends to a user’s friend list
-}
+    deleteFriend({ params, body }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId},
+            { $pull: { friends: { friendId: body.friendId}}},
+            { runValidators: true, new: true }
+        )
+        .then(user => 
+            !user
+            ?res.status(404).json({ message: 'Sorry but there is no Friend with this ID'})
+            : res.json(user))
+            .catch((err) => res.status(500).json(err));
+    }
+};
